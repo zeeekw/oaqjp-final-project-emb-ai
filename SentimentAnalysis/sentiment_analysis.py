2b83face5b226354491781a89ab854f96c6337da
zeeekw <zeee.21@hotmail.com>
2026-09-12 19:49:21 -0400

    Add error handling for invalid input (Task 7)

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -6,7 +6,14 @@ def sentiment_analyzer(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     response = requests.post(url, json = myobj, headers=header)
-    formatted_response = json.loads(response.text)
-    label = formatted_response['documentSentiment']['label']
-    score = formatted_response['documentSentiment']['score']
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    elif response.status_code == 500:
+        label = None
+        score = None
+    else:
+        label = None
+        score = None
     return {'label': label, 'score': score}
--- server.py
+++ server.py
@@ -5,11 +5,22 @@ app = Flask("Sentiment Analyzer")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
-    text_to_analyze = request.args.get('textToAnalyze')
-    response = sentiment_analyzer(text_to_analyze)
-    label = response['label']
-    score = response['score']
-    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+	# Retrieve the text to analyze from the request arguments
+	text_to_analyze = request.args.get('textToAnalyze')
+
+	# Pass the text to the sentiment_analyzer function and store the response
+	response = sentiment_analyzer(text_to_analyze)
+
+	# Extract the label and score from the response
+	label = response['label']
+	score = response['score']
+
+	# Check if the label is None, indicating an error or invalid input
+	if label is None:
+		return "Invalid input! Try again."
+	else:
+		# Return a formatted string with the sentiment label and score
+		return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
 
 @app.route("/")
 def render_index_page():
